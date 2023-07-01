0a14245a42ae54b391bb7c76e07ae086fd5dae51
Tony Stark <z.zedaniam@gmail.com>
2023-07-01 14:36:17 +0530

    Fix message :)

--- dz_server.lua
+++ dz_server.lua
@@ -4,10 +4,9 @@ Config.Used = false -- Don't touch.
 
 ESX.RegisterCommand({'announce', 'annc'}, 'admin', function(xPlayer, args, showError)
     if not Config.Used then
-        TriggerClientEvent('dz-announce:client:sendNuiMessage', -1, Config.Length, args.text)
+        TriggerClientEvent('dz-announce:client:sendNuiMessage', -1, Config.Length, table.concat(args, ' '))
         Config.Used = true
         Wait(Config.Length + 1000)
         Config.Used = false
     end
-  end, false, {help ='Send an Announcement', arguments = {{name = 'text', help = 'Message', type = 'string'}}
-})
+end, true)
